21ea35c51aabb98d29721cd5f8cd015295515ecc
Nick Schulte <nick8012@hotmail.com>
2026-05-01 14:00:52 -0500

    Updating API/src/index.ts file to support production environment.

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import contactsRouter from './routes/contactsRouter';
 
 const app = express();
+const isProd = process.env.NODE_ENV === 'production';
 
 app.use(cors({
     origin: process.env.ALLOWED_ORIGIN,
@@ -16,4 +18,12 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/contacts', contactsRouter);
 
-app.listen(3000, () => console.log('API running on http://localhost:3000'));
+if(isProd) {
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+    app.get('*{path}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+    })
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('API running on port ${port}'));
